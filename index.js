d135ed203aef8231377254fc0b9409df43fd53e4
James <jamesschmidt124@gmail.com>
2021-09-22 14:40:04 -0700

    commit tree ba745aa14c2c0fd891751f7b43cd7c4340b55fd6

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
+// superbowlWin = (year, result) => {
+//     if (result === "W")
+//     return year 
+// }learn
 
+// let superbowlWin = record.find(function(record){ 
+//     if (record.result === "W")
+//        return (record.find(year));
+// }
+
+
+// console.log(superbowlWin)
+//   const win = array.find(function(array) {
+//     return array["result"] === "W";
+//     )
+//     if (win === undefined) {
+//         return undefined
+//     } else {
+//         return win["year"] ;
+//     }
+//}
+// let superbowlWin = record.find(isW)
+// console.log(superbowlWin)
+
+// function isW(record) {
+//     return (record["result"] === "W")
+// }
+// function year(record){
+//     return (record["year"])
+// }
+// function superbowlWin("year") {
+//     for (const record of year) {
+//         console.log(record.year)
+//     }
+// }
+// superbowlWin(year);
+function superbowlWin(array) {
+   array.find(["result"] === "W");
+   return array; {
+   }   
+}
